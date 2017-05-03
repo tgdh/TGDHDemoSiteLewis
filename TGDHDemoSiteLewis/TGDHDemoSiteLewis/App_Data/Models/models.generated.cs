90b288ec22c01c51f8ee95ee105b6d05bdb7d239
Lewis Smith <LewisS303@hotmail.co.uk>
2017-05-03 11:51:32 +0100

    Build

--- TGDHDemoSiteLewis/TGDHDemoSiteLewis/App_Data/Models/models.generated.cs
+++ TGDHDemoSiteLewis/TGDHDemoSiteLewis/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "11ac61850f50f32c")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
